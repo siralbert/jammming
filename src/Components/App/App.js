e2bc7d89edd257bd2e60a9e7789fd863fd216c54
Albert <albert.tsai21@gmail.com>
2019-10-03 15:28:48 -0500

    Added initial directory structure.  Moved App.css and App.js to appropriate folder.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './reset.css';
+import '../../reset.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

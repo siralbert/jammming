a1d61241442e7192640f3690ca99380418f92e21
Albert <albert.tsai21@gmail.com>
2019-10-04 15:37:30 -0500

    Add Tracks to a Playlist and Remove Tracks from a Playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -20,6 +20,25 @@ class App extends React.Component {
                        {name:'playlistName3',artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6}]
       
     }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track){
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks});
   }
 
   render(){
@@ -29,8 +48,8 @@ class App extends React.Component {
               <div className="App">
                   <SearchBar />
                   <div className="App-playlist">
-                      <SearchResults searchResults={this.state.searchResults} />
-                      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />                 
+                      <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+                      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />                 
                   </div>
               </div>
         </div>
